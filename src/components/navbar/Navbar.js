0bade770438cb84251f520d91b25654f65ca5a16
alimh86672018 <alimh86672018@gmail.com>
2019-11-15 23:28:24 +0330

    navbar img changed

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.scss";
-import logo from "../../logo2.png";
+import logo from "../../logo3.png";
 
 function Navbar() {
   return (
--- src/components/tourList/tourData.js
+++ src/components/tourList/tourData.js
@@ -4,27 +4,31 @@ export const tourData = [
     city: "new york",
     name: "new york bridge tour",
     img: "./img/newyork.jpg",
-    info: "lorem afsf kjwoiurw dljaf ckjfj lsla qrufi cnnzn ;auwonql alljnvnmbzl lal lzljh oqruqo faf",
+    info:
+      "Extending for 1.3 miles (2 kilometers) across New York City’s East River, this 19th-century bridge sees constant foot, bike, and car traffic thanks to commuters and sightseers alike."
   },
   {
     id: 2,
     city: "london",
     name: "london royal palace tour",
     img: "./img/london.jpg",
-    info: "lorem afsf kjwoiurw dljaf ckjfj lsla qrufi cnnzn ;auwonql alljnvnmbzl lal lzljh oqruqo faf",
+    info:
+      "If you were a king or queen for the day in London, what would you do? Ride around in a carriage made out of gold, wear the world's most famous diamond on your head"
   },
   {
     id: 3,
     city: "paris",
     name: "paris lights tour",
     img: "./img/paris.jpg",
-    info: "lorem afsf kjwoiurw dljaf ckjfj lsla qrufi cnnzn ;auwonql alljnvnmbzl lal lzljh oqruqo faf",
+    info:
+      "What could be better than seeing the City of Lights all lit up for the holidays? Enjoy a Christmas-themed tour of Paris to see this incredible city illuminated."
   },
   {
     id: 4,
     city: "tokyo",
     name: "tokyo sushi tour",
     img: "./img/tokyo.jpg",
-    info: "lorem afsf kjwoiurw dljaf ckjfj lsla qrufi cnnzn ;auwonql alljnvnmbzl lal lzljh oqruqo faf",
+    info:
+      "Japan is known for its fresh seafood, and this small-group tour explores the outer Tsukiji Fish Market, where Tokyo's top chefs shop for high-quality fish. Sample a range of sushi, Japanese-style omelettes, fried fish cakes, oysters, sake, and Japanese tea on this in-depth experience of the legendary market."
   }
-]
+];
